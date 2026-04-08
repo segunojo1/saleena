7218508cc266b2d33786aadd794003d9f8590959
Segun Ojo <olusegunkd2017@gmail.com>
2026-04-08 16:08:22 +0100

    update to motion

--- components/Sticker.tsx
+++ components/Sticker.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { Heart, Star, Flower, Sparkles, Globe, Mail, Sun, Headphones, Smile } from "lucide-react";
 import { cn } from "../lib/utils";
 
